6b8c6ad80d1d49cdf8854a7a221efbc1f1bcdf1a
DOHHAN <101185804+DOHHAN@users.noreply.github.com>
2024-04-11 16:42:56 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,6 +1,27 @@
 #include<stdio.h>
+#define acracy double
+
+#define gap 10000
+
 int main(void)
 {
-    printf("dd");
-    return 0;
+	acracy Mtau = 3;
+	acracy zeta[gap];
+	acracy difzeta[gap];
+	acracy dtau = (Mtau) / (gap-1);
+	zeta[0] = 0.9;
+	zeta[1] = 0.9;
+	difzeta[0] = 0;
+	for(int i=0; i<gap-2; i++)
+	{
+		acracy dif2zeta = (1 / zeta[i + 1] / zeta[i + 1] / zeta[i + 1]) - (1 / zeta[i + 1] / zeta[i + 1]);
+		zeta[i + 2] = dif2zeta * dtau * dtau + 2 * zeta[i + 1] - zeta[i];
+	}
+	for(int i=0; i<gap;i++)
+	{
+		printf("num %d zeta %f leng %f \n",i+1 ,(float)zeta[i],(float)(i*dtau) );
+	}
+
+
+	return 0;
 }
